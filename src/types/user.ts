f455c16e8e95a1506e2a57ad4db0c059d408975f
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2026-01-08 06:07:10 +0530

    update zod types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,15 +1,7 @@
 import * as z from "zod";
 
-export const UserSchema = z.object({
+const MemberSchema = z.object({
   name: z.string().regex(/^[a-zA-Z\s]{2,32}$/, "Please enter a valid name"),
-  email: z
-    .email("Please enter a valid email address")
-    .min(1, "Email is required"),
-  phone_number: z
-    .string()
-    .regex(/^[+]?[\d\s\-()]+$/, "Please enter a valid phone number")
-    .min(10, "Phone number must be at least 10 digits"),
-  gender: z.enum(["Male", "Female"], "Please select a valid gender"),
   batch: z.enum(
     ["25.3", "25.2", "25.1", "24.3", "24.2", "24.1", "23.2", "23.1"],
     "Please select a valid batch"
@@ -28,7 +20,24 @@ export const UserSchema = z.object({
     ],
     "Please select a valid degree"
   ),
-  isMember: z.stringbool("Please select your status"),
 });
 
-export type UserType = z.infer<typeof UserSchema>;
+export const TeamSchema = z.object({
+  team_name: z.string().min(2, "Team name must be at least 2 characters"),
+  team_email: z
+    .string()
+    .email("Please enter a valid email address")
+    .min(1, "Email is required"),
+  team_phone_number: z
+    .string()
+    .regex(/^[+]?[\d\s\-()]+$/, "Please enter a valid phone number")
+    .min(10, "Phone number must be at least 10 digits"),
+  isMember: z.boolean("Please select your status"),
+  members: z
+    .array(MemberSchema)
+    .min(3, "A team must have at least 3 members")
+    .max(4, "A team can have at most 4 members"),
+});
+
+export type TeamType = z.infer<typeof TeamSchema>;
+export type MemberType = z.infer<typeof MemberSchema>;
